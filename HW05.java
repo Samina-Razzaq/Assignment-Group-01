c262e9cf5e0a3b2dcbca0c0fd98a5a24c4d2698f
Samina-Razzaq <s.razzaq2012@gmail.com>
2022-02-16 22:27:40 -0800

    1st commit

---
+++ HW05.java
@@ -0,0 +1,23 @@
+package groupAssignment;
+
+public class HW05 {
+
+	public static void main(String[] args) {
+		
+		
+		//Write a program to swap 2 numbers without a temporary variable?
+		
+		int i=10;
+		int j=20;
+		
+		
+		i=i+j;
+		j=i-j;
+		i=i-j;
+		
+		System.out.println("Values of i and j after swaping are "+i+" and "+j);
+
+
+	}
+
+}
